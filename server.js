ed6e7925b246591de88bbd788b35fc6e1ce584fa
Yuna Pahk <new1@new1s-MacBook-Pro.local>
2023-08-27 02:29:08 -0700

    first commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
+ 
 //////////////////////
 //DATABASE CONNECTION
 //////////////////////
