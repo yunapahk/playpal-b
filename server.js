c79bb1f5390fce6211c18dfd3608bcb53f8c67a1
Yuna Pahk <yunapahk@gmail.com>
2024-04-08 12:14:47 -0700

    should work now

--- server.js
+++ server.js
@@ -1,48 +1,32 @@
-//////////////////////
 // IMPORT DEPENDENCIES
-//////////////////////
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const app = express();
-require('./config/db');
-const PORT = process.env.PORT || 4567;
-
-// Use CORS with options
-app.use(cors(corsOptions));
-
-// Parse JSON bodies (as sent by API clients)
-app.use(express.json());
 
 // Database connection
 require('./config/db');
+const PORT = process.env.PORT || 4567;
 
-//////////////////////
 // IMPORT MIDDLEWARE
-//////////////////////
 const { middlewareSetup, authCheck } = require('./middleware/mid');
+// Apply centralized middleware setup
 middlewareSetup(app);
 
-//////////////////////
 // IMPORT ROUTES
-//////////////////////
 const dogRoutes = require('./routes/dogRoutes');
 const authRoutes = require('./routes/authRoutes');
 
-//////////////////////
 // SETUP ROUTES
-//////////////////////
 app.get('/', (req, res) => {
     res.json({hello: "server"});
 });
 
-// Auth related routes
-app.use(authRoutes);
+// Use middleware as needed, for example, authCheck for secure routes
+// app.use('/secure-route', authCheck, secureRouteHandler);
 
-// Dog related routes (can be prefixed if desired e.g. app.use("/dogs", dogRoutes); )
+// Apply routes
+app.use(authRoutes);
 app.use(dogRoutes);
 
-//////////////////////
 // LISTENER
-//////////////////////
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
